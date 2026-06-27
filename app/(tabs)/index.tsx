3960a3d001c2090502b2cd6e76d23e560020e640
psycopathic <harshsrivastava2502@gmail.com>
2026-06-28 02:43:33 +0530

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Harsh Shrivastava</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
